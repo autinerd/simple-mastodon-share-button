e8078ca208e3c54f7c8f10f8a7f9009a2d5d6169
autinerd <27780930+autinerd@users.noreply.github.com>
2022-10-30 10:35:31 +0100

    Show URL in instance prompt

--- mastodonShare.js
+++ mastodonShare.js
@@ -1,14 +1,14 @@
 function mastodonShareButtonClick(e) {
 	const btn = e.target;
-	const mastodonInstance = prompt('Please enter your Mastodon instance (e.g. mastodon.social) for sharing');
-	if (mastodonInstance == null) {
-		return;
-	}
 	let href = document.URL;
 	let title = document.title;
 	if ('data-title' in btn.attributes && 'data-href' in btn.attributes) {
 		href = btn.attributes['data-href'].value;
 		title = btn.attributes['data-title'].value;
+	}
+	const mastodonInstance = prompt(`Sharing "${href}"\nPlease enter your Mastodon instance (e.g. mastodon.social) for sharing`);
+	if (mastodonInstance == null) {
+		return;
 	}
 	if (mastodonInstance.indexOf('/') === -1) {
 		window.open('https://' + mastodonInstance + '/share?text=' + encodeURIComponent(title) + ' ' + encodeURIComponent(href), "_blank");
